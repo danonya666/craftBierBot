0fa18db180cb2f16ea27daf2ea175597e3abdf43
Daniil <32582330+danonya666@users.noreply.github.com>
2018-11-10 14:20:23 +0300

    Add files via upload
    added logging

--- bot.py
+++ bot.py
@@ -1,17 +1,33 @@
 import telebot
 import telegram
+
 import config
 import requests
 import logging
+import datetime
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
 import ssl
 from threading import Thread, Lock, current_thread, Event
 from time import sleep
 
-from telegram import Bot, TelegramError
+from telegram import Bot, TelegramError, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Dispatcher, JobQueue, CommandHandler, MessageHandler, Filters
 from telegram.utils.request import Request
+
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
 
 def get_updates_json(request):
     response = requests.get(request + "getUpdates")
@@ -34,32 +50,61 @@ def send_message(chat, text):
     response = requests.post(config.url + 'sendMessage', data=params)
     return response
 
+
 def start_callback(bot, update):
     update.message.reply_text('WELCOME SIR')
-    print("I was here")
+    button_list = [InlineKeyboardButton("Извиниться", switch_inline_query_current_chat="Прости пожалуста"),
+                   InlineKeyboardButton("Обидеться", switch_inline_query_current_chat="Я обиделась!")
+                   ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
+    bot.send_message(chat_id=update.message.chat_id, text="A two-column menu", reply_markup=reply_markup)
+
+    # LOGGING
+    print("----------------------------")
+    print(datetime.datetime.now())
+    print(update.effective_user.username)
+    print(update.effective_message.text)
+    print("----------------------------")
+
 
 def message_response(bot, update):
-    bot.send_message(chat_id= update.message.chat_id, text= "Я глупый бот, пока что не умею разговаривать, но скоро "
-                                                            "обязательно научусь!")
+    if update.effective_message.text == "@CraftBierBot Прости пожалуста":
+        bot.send_message(chat_id=update.message.chat_id, text="Я уезжаю в ПРАГУ!")
+    elif update.effective_message.text == "@CraftBierBot Я обиделась!":
+        bot.send_message(chat_id=update.message.chat_id, text="Не обижайся дорогая, поехали в ПРАГУ!")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text="Чего изволите, мой Господин?")
+    print("----------------------------")
+    print(datetime.datetime.now())
+    print(update.effective_user.username)
+    print(update.effective_message.text)
+    print("----------------------------")
+    log_msg_to_file(update, "bot_log.txt")
 
 
+def log_msg_to_file(update, file):
+    f = open(file, 'a')
+    f.write("----------------------------" + "\n")
+    f.write(str(datetime.datetime.now()) + "\n")
+    f.write(update.effective_user.first_name + " ")
+    f.write(update.effective_user.last_name + "(" + update.effective_user.username + ")" + "\n")
+    f.write(update.effective_message.text + "\n")
+    f.write("----------------------------" + "\n")
 
 
 def main():
-
     url = '190.15.195.64'
     port = '47912'
     TOKEN = config.api_token
     REQUEST_KWARGS = {
-        'proxy_url': 'http://190.53.46.14:56450/' # NEED TO FIND NORMAL PROXY
-        #'proxy_url': 'http://' + url + ':' + port + '/',
+        'proxy_url': 'http://69.70.219.202:56946/'  # NEED TO FIND NORMAL PROXY
+        # 'proxy_url': 'http://' + url + ':' + port + '/',
         # Optional, if you need authentication:
         # 'username': 'PROXY_USER',
         # 'password': 'PROXY_PASS',
     }
-
+    
     updater = telegram.ext.Updater(TOKEN, request_kwargs=REQUEST_KWARGS)
-    bot = telebot.TeleBot(config.api_token)
     start_handler = CommandHandler('start', start_callback)
     message_handler = MessageHandler(Filters.text, message_response)
     updater.dispatcher.add_handler(start_handler)
@@ -67,8 +112,6 @@ def main():
     updater.start_polling()
     updater.idle()
 
+
 if __name__ == '__main__':
     main()
-
-
-
